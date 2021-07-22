816a0c303a10efd2eefb1b8da51b6266df8f9f56
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-22 17:36:58 -0300

    feat: Agregando componente para tomar grupo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="">
     <h1 class="text-white text-center text-3xl pt-1">Alumno Chathink</h1>
+    <div class="flex mt-10 mx-5">
+      <Group />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapActions } from "vuex";
+import Group from '@/components/Group.vue';
 
 export default {
   name: "Home",
-  methods: {
-    ...mapActions(["syncToken", "checkSession"]),
+  components: {
+    Group
   },
   created() {
     this.syncToken();
     this.checkSession();
+  },
+  methods: {
+    ...mapActions(["syncToken", "checkSession"]),
+    
   },
 };
 </script>
