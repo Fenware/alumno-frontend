fd5dd940f5fd83a0a81ec3ca7a8b53925c69af0f
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-25 15:49:44 -0300

    refactor: Quitando if de un div innecesario

--- src/components/ConsultationsContainer.vue
+++ src/components/ConsultationsContainer.vue
@@ -1,29 +1,28 @@
 <template>
   <div
+    class="mb-3 max-w-xs text-white rounded-xl pb-2 | bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-xl"
     v-for="consultation in consultations"
     :key="consultation.id"
   >
-    <div v-if="parseInt(consultation.state) == 1" class="mb-3 max-w-xs text-white rounded-xl pb-2 | bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-xl">
-      <div class="flex flex-wrap pt-1">
-        <span
-          class="ml-2 text-xs bg-blue-300 px-1 text-black rounded-md shadow-lg select-none"
-          >{{ consultation.teacher_name }}</span
-        >
-        <span
-          class="ml-2 text-xs bg-purple-800 px-1  rounded-md shadow-lg select-none"
-          >{{ consultation.subject_name }}</span
-        >
-      </div>
-      <p class="text-sm px-4 pt-1">
-        {{ consultation.theme }}
-      </p>
-      <button
-        @click="viewConsultation(consultation)"
-        class="mt-1 ml-3 text-xs bg-white bg-opacity-20 hover:bg-opacity-30 hover:shadow-xl transition-all ease-linear px-2 py-0.5 rounded-full"
+    <div class="flex flex-wrap pt-1">
+      <span
+        class="ml-2 text-xs bg-blue-300 px-1 text-black rounded-md shadow-lg select-none"
+        >{{ consultation.teacher_name }}</span
       >
-        Ver consulta
-      </button>
+      <span
+        class="ml-2 text-xs bg-purple-800 px-1  rounded-md shadow-lg select-none"
+        >{{ consultation.subject_name }}</span
+      >
     </div>
+    <p class="text-sm px-4 pt-1">
+      {{ consultation.theme }}
+    </p>
+    <button
+      @click="viewConsultation(consultation)"
+      class="mt-1 ml-3 text-xs bg-white bg-opacity-20 hover:bg-opacity-30 hover:shadow-xl transition-all ease-linear px-2 py-0.5 rounded-full"
+    >
+      Ver consulta
+    </button>
   </div>
 </template>
 
