f699dca673ab6f61e05c745eb77bd879e4f1128c
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-08 21:12:54 -0300

    feat: Vista de profesores

---
+++ src/views/Teachers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2 class="text-white text-center text-3xl font-semibold pt-1">
+      Tus profesores
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Teachers"
+}
+</script>
+
+<style>
+
+</style>
