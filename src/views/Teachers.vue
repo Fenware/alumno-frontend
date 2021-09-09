4291a371e4207c22af8e7f663a2543e497aaf238
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-08 22:49:46 -0300

    feat: Obteniendo profesores

--- src/views/Teachers.vue
+++ src/views/Teachers.vue
@@ -3,15 +3,28 @@
     <h2 class="text-white text-center text-3xl font-semibold pt-1">
       Tus profesores
     </h2>
+    <div></div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 export default {
-  name: "Teachers"
-}
+  name: "Teachers",
+  computed: {
+    ...mapState({
+      teachers: (state) => state.teachers.teachers,
+    }),
+  },
+  created() {
+    this.getUserGroup().then(() => {
+      this.getTeachers();
+    });
+  },
+  methods: {
+    ...mapActions(["getTeachers", "getUserGroup"]),
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
