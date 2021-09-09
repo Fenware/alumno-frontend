880e7c61b0055972ebde7feb8066ea7058b71a38
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-08 21:09:41 -0300

    refactor: Estilos + iconos de google en el nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,49 @@
 <template>
   <nav
-    class="w-full sm:w-1/6 md:w-16 lg:w-16 sm:h-full rounded-2xl | bg-gray-700 border-r-2 border-t-2 border-b-2 border-gray-700  bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl"
+    class="w-full sm:w-1/6 md:w-16 lg:w-16 sm:h-full rounded-2xl bg-gray-700 bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl"
   >
     <ul
-      class="p-2 sm:py-4 sm:px-0 h-full text-center flex flex-row sm:flex-col justify-between"
+      class="p-2 sm:py-4 sm:px-0 h-full text-center flex flex-row sm:flex-col justify-between items-center"
     >
-      <div class="flex gap-2 sm:gap-0 sm:block">
+      <div class="flex gap-2 sm:gap-0 sm:block  items-center">
 
         <li>
-          <router-link :to="{ name: 'Home' }" class="cursor-pointer">
-            <i :class="'fa-home ' + icon_class"></i>
+          <router-link :to="{ name: 'Home' }"  v-slot="{ isActive }">
+            <span :class="[isActive && 'active']" class="nav-icon mt-1 material-icons">
+              home
+            </span>
           </router-link>
         </li>
 
         <li>
-          <router-link :to="{ name: 'ChatRooms' }" class="cursor-pointer">
-            <i :class="'fa-comments ' + icon_class"></i>
+          <router-link :to="{ name: 'ChatRooms' }" v-slot="{ isActive }">
+            <span :class="[isActive && 'active']" class="nav-icon mt-1 material-icons">
+              question_answer
+            </span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'Teachers' }" v-slot="{ isActive }">
+            <span :class="[isActive && 'active']" class="nav-icon mt-1 material-icons">
+              people
+            </span>
           </router-link>
         </li>
 
       </div>
-      <div class="flex gap-2 sm:gap-0 sm:block">
-        <li class="cursor-pointer">
-          <router-link :to="{ name: 'Configuration' }" class="cursor-pointer">
-            <i :class="'fa-cog ' + icon_class"></i>
+      <div class="flex gap-2 sm:gap-0 sm:block  items-center">
+        <li>
+          <router-link :to="{ name: 'Configuration' }" v-slot="{ isActive }">
+            <span :class="[isActive && 'active']" class="nav-icon mt-1 material-icons">
+              manage_accounts
+            </span>
           </router-link>
         </li>
         <li class="cursor-pointer">
-          <i
-            @click="logout()"
-            :class="'fa-sign-out-alt  mb-4 ' + icon_class"
-          ></i>
+          <span @click="logout()" class="nav-icon material-icons">
+            logout
+          </span>
         </li>
       </div>
     </ul>
@@ -41,16 +54,17 @@
 import { mapActions } from "vuex";
 export default {
   name: "Navbar",
-  data: function() {
-    return {
-      icon_class:
-        "fas sm:mt-6 text-3xl text-white transition-colors transition-transform duration-500 ease-in-out filter hover:text-gray-200 transform hover:scale-110 drop-shadow-lg",
-    };
-  },
   methods: {
     ...mapActions(["logout"]),
   },
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.nav-icon {
+  @apply mt-0 px-2 py-1 sm:mt-2 text-3xl text-white transition-colors ease-in-out hover:bg-gray-600 rounded-xl;
+}
+.active{
+  @apply bg-gray-600 transition-colors shadow-lg;
+}
+</style>
