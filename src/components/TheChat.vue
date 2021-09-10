b8af595ddc353757a71075c8da38ae12131060da
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-10 12:57:45 -0300

    feat: Modal para ver info de chat

--- src/components/TheChat.vue
+++ src/components/TheChat.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-col gap-1 justify-between h-full w-full">
-    <div class="mt-1 flex flex-col justify-between h-full">
-      <div class="select-none">
+    <div class="flex mt-1 flex-col justify-between h-full">
+      <div
+        @click="openModal()"
+        class="pb-2 w-9/12 mx-auto hover:bg-gray-700 rounded-xl transition-colors cursor-pointer select-none"
+      >
         <p class="text-3xl text-center">
           {{ chat != null ? chat.subject_name : "Esperando datos" }}
         </p>
@@ -12,7 +15,8 @@
 
       <div
         id="chat"
-        class="flex gap-1 flex-col pl-5 pr-12 max-h-96 overflow-y-auto mx-5 xl:w-9/12 xl:mx-auto"
+        class="flex gap-1 flex-col pl-5 pr-12 overflow-y-auto mx-5 xl:w-9/12 xl:mx-auto"
+        style="max-height: 70vh;"
       >
         <div
           v-for="(message, index) in chat.messages"
@@ -82,13 +86,81 @@
         <span class="material-icons ">send</span>
       </div>
     </div>
-    <!-- 
-    </input> -->
+
+    <div
+      class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden hidden justify-center items-center animated fadeIn faster rounded-2xl"
+      style="background: rgba(0,0,0,.5);"
+    >
+      <div
+        style="max-height: 65vh;"
+        class="border-2 border-gray-500 modal-container bg-gray-900 w-11/12 md:max-w-md mx-auto rounded-xl shadow-lg z-50 overflow-y-auto"
+      >
+        <div class="modal-content py-4 text-left px-6">
+          <!--Title-->
+          <div class="flex justify-between items-center">
+            <p class="text-2xl font-bold">{{ chat.subject_name }}</p>
+            <div class="modal-close cursor-pointer z-50" @click="modalClose()">
+              <span class="material-icons text-red-400">close</span>
+            </div>
+          </div>
+          <!--Body-->
+          <div class="my-1">
+            <p class="text-lg font-medium mb-2">{{ chat.theme }}</p>
+            <hr />
+            <p class="mt-2 text-right">
+              <span class="text-sm"> {{ getDate(chat.creation_date) }}</span>
+            </p>
+            <p class="">
+              Grupo:
+              <span class="font-medium text-indigo-400">{{
+                chat.group_name
+              }}</span>
+            </p>
+            <p class="">
+              Creador:
+              <span class="font-medium text-indigo-400">
+                {{ chat.student_name }}</span
+              >
+            </p>
+            <p class="">
+              Docente:
+              <span class="font-medium text-indigo-400">
+                {{ chat.teacher_name }}</span
+              >
+            </p>
+
+            <p class="mt-5 font-medium text-lg text-center">Participantes</p>
+
+            <div>
+              <ol class="overflow-y-auto max-h-28">
+                <li v-for="participant in chat.participants" :key="participant">
+                  -
+                  {{ participant.name }}
+                  {{ participant.surname }}
+                  {{ participant.second_surname }}
+                </li>
+              </ol>
+            </div>
+          </div>
+          <!--Footer-->
+          <div class="flex justify-center pt-2 mt-2">
+            <button
+              v-if="user.id == chat.id_student"
+              @click="closeChat()"
+              class="btn-danger flex items-center py-0.5"
+            >
+              <span class="material-icons-round mr-1">warning_amber</span>
+              Terminar sala de chat
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
 import moment from "moment";
 
 export default {
@@ -107,8 +179,18 @@ export default {
   updated() {
     this.scroll();
   },
+  unmounted() {
+    // Borrando el chat al salir del componente
+    this.clearChat();
+  },
   methods: {
-    ...mapActions(["sendMessageToChat"]),
+    ...mapActions([
+      "sendMessageToChat",
+      "closeChatRoom",
+      "getUserData",
+      "getChatRoomById",
+    ]),
+    ...mapMutations(["clearChat"]),
     sendMessage() {
       this.sendMessageToChat({ id: this.chat.id, message: this.new_message });
       this.new_message = "";
@@ -117,6 +199,13 @@ export default {
       // Formateo la fecha a español
       let date_formated = moment(date).locale("es");
       date_formated = moment(date_formated).format("LT");
+      /* return moment(date_formated).format('LT'); */
+      return date_formated;
+    },
+    getDate(date) {
+      // Formateo la fecha a español
+      let date_formated = moment(date).locale("es");
+      date_formated = moment(date_formated).format("LL");
       /* return moment(date_formated).format('LT'); */
       return date_formated;
     },
@@ -135,8 +224,95 @@ export default {
         return false;
       }
     },
+    openModal() {
+      this.getChatRoomById(this.chat.id).then(() => {
+        let modal = document.querySelector(".main-modal");
+        modal.classList.remove("fadeOut");
+        modal.classList.add("fadeIn");
+        modal.style.display = "flex";
+      });
+    },
+    modalClose() {
+      let modal = document.querySelector(".main-modal");
+      modal.classList.remove("fadeIn");
+      modal.classList.add("fadeOut");
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 500);
+    },
+    closeChat() {
+      let alert = this.$swal.mixin({
+        toast: false,
+        position: "center",
+        showConfirmButton: true,
+        showDenyButton: true,
+        timer: 60000,
+        timerProgressBar: true,
+        iconColor: "white",
+        heightAuto: true,
+        customClass: {
+          popup: "colored-toast",
+        },
+      });
+      alert
+        .fire({
+          html: `<span class="text-white">¿Estas seguro de terminar la sala de chat?</span>`,
+          showCancelButton: false,
+          confirmButtonText: `Confirmar`,
+          denyButtonText: `Cancelar`,
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.closeChatRoom({ chat: parseInt(this.chat.id) });
+            this.modalClose();
+            this.clearChat();
+          }
+        });
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.animated {
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+.animated.faster {
+  -webkit-animation-duration: 500ms;
+  animation-duration: 500ms;
+}
+
+.fadeIn {
+  -webkit-animation-name: fadeIn;
+  animation-name: fadeIn;
+}
+
+.fadeOut {
+  -webkit-animation-name: fadeOut;
+  animation-name: fadeOut;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+}
+</style>
