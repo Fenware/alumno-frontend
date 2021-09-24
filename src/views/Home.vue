d0ed7dbcbb70a33a90cd9ac112afb74908e54a6a
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-23 23:56:22 -0300

    refactor: Quitando consultas de home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="">
-    <h1 class="text-white text-center text-3xl pt-1">Alumno Chathink</h1>
+    <h1 class="text-white text-center text-3xl font-semibold pt-1">Alumno Chathink</h1>
     <div class="flex flex-wrap gap-4 mt-10 mx-5">
       <div><Group /></div>
-      <div>
-        <CreateConsultation />
-      </div>
     </div>
   </div>
 </template>
@@ -14,13 +11,11 @@
 // @ is an alias to /src
 import { mapActions } from "vuex";
 import Group from "@/components/Group.vue";
-import CreateConsultation from "@/components/CreateConsultation.vue";
 
-export default {
+export default {  
   name: "Home",
   components: {
     Group,
-    CreateConsultation,
   },
   created() {
     this.getUserGroup();
