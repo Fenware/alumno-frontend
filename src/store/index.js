8cdf566abf05aaf0a7e8816e4e0ab98f1ce81b9c
lucaspintos909 <lucaspintos909@gmail.com>
2021-10-06 11:01:33 -0300

    refactor: Moviendo funciones y state a modulo vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore } from "vuex";
-import axios from "axios";
 
 /* Modulos */
 import auth from "./modules/auth";
@@ -7,6 +6,7 @@ import chatRooms from "./modules/chatRooms";
 import userProfile from "./modules/userProfile";
 import teachers from "./modules/teachers";
 import consultations from "./modules/consultations";
+import group from "./modules/group";
 
 export default createStore({
   modules: {
@@ -15,24 +15,10 @@ export default createStore({
     userProfile,
     teachers,
     consultations,
+    group
   },
   state: {
     API_URL: process.env.VUE_APP_ROOT_API,
-    group: null,
-    subjects: null,
-    consultation: {
-      creation_date: "Esperando datos",
-      group_name: "Esperando datos",
-      id: "Esperando datos",
-      state: "1",
-      subject_name: "Esperando datos",
-      teacher_name: "Esperando datos",
-      theme: "Esperando datos",
-      body: "Esperando datos",
-      messages: [],
-    },
-    new_message: "",
-    new_message_mode: false,
     token: null,
     headers: {
       Authorization: "",
@@ -50,54 +36,10 @@ export default createStore({
     setText(state, payload) {
       state.text_filter = payload;
     },
-    setGroup(state, group) {
-      state.group = group;
-    },
-    setUserSubjects(state, subjects) {
-      state.subjects = subjects;
-    },
   },
   actions: {
     searcher({ commit }, payload) {
       commit("setText", payload.toLowerCase());
-    },
-    async getUserGroup({ state, dispatch, commit }) {
-      await axios({
-        method: "post",
-        url: state.API_URL + "/user-group/getGroups",
-        headers: state.headers,
-      })
-        .then((res) => {
-          if (Array.isArray(res.data)) {
-            commit("setGroup", res.data[0]);
-            if (state.subjects == null) {
-              dispatch("getUserSubjects");
-            }
-            /* dispatch("wsChatRoomsConnection"); */
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    async getUserSubjects({ state, commit }) {
-      let data = {
-        group: parseInt(state.group.id_group),
-      };
-      await axios({
-        method: "post",
-        url: state.API_URL + `/user-group/getGroupSubjects`,
-        data,
-        headers: state.headers,
-      })
-        .then((res) => {
-          if (Array.isArray(res.data) && res.data.length > 0) {
-            commit("setUserSubjects", res.data);
-          }
-        })  
-        .catch((error) => {
-          console.log(error);
-        });
     },
   },
   getters: {
