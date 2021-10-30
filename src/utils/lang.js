de5c595ac23f5c26c4025c7a1f8234a748302752
MateConAzucar <fds.workmail@gmail.com>
2021-10-30 16:22:20 -0300

    fix: removed non used and added used jsons

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -1,17 +1,14 @@
 import lang_j from '@/assets/json_lang/lang.json';
 import login_j from '@/assets/json_lang/login.json';
 import error_j from '@/assets/json_lang/error.json';
-import group_j from '@/assets/json_lang/group.json';
 import register_j from '@/assets/json_lang/register.json';
 import teacher_j from '@/assets/json_lang/teacher.json';
 import user_j from '@/assets/json_lang/user.json';
 import consultation_j from '@/assets/json_lang/consultation.json';
-
+import chat_j from '@/assets/json_lang/chat.json';
 
 export function getWord({word,file,lang}){
     let palabra = "???";
-    console.log(palabra);
-
     switch(file){
         case 'lang':
             palabra = lang_j[word][lang];
@@ -21,9 +18,6 @@ export function getWord({word,file,lang}){
             break;
         case 'error':
             palabra = error_j[word][lang];
-            break;
-        case 'group':
-            palabra =  group_j[word][lang];
             break;
         case 'teacher':
             palabra =  teacher_j[word][lang];
@@ -37,10 +31,14 @@ export function getWord({word,file,lang}){
         case 'consultation':
             palabra =  consultation_j[word][lang];
             break;
+        case 'chat':
+            palabra =  chat_j[word][lang];
+            break;
         default:
             palabra = "{missing}";
             break;
     }
     palabra = palabra ? palabra : "{missing}";
+    //console.log(palabra);
     return palabra;
 }
